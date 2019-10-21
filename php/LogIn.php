0d58bf27dee83e4e5aab1e784b042558c83b90e1
nachobf <nachobarriocanal@gmail.com>
2019-10-21 18:03:59 +0200

    formulario login

--- php/LogIn.php
+++ php/LogIn.php
@@ -10,6 +10,12 @@
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
+		<div style="border-style:solid;border-color:black; font-family: Verdana,Geneva,sans-serif; text-align:center; width:50%; margin-left:auto; margin-right:auto;"> <p style="text-align:center;">INICIO DE SESIÓN</p><br>
+		<form method="POST" action="AddQuestionWithImage.php" id='fquestion' name='fquestion' enctype="multipart/form-data" accept-charset="UTF-8">
+			Email*: <br><input style="text-indent: 1em" type="text" id="email" name="email" size=35><br>
+			Contraseña*: <br><input type="password" id="password" name="password" size=35><br><br>
+			<p style="text-align:center;"><input type="submit" id="enviar" name="enviar" value="Enviar"></p>	
+		</form>
 	</div>
   </section>
   <?php include '../html/Footer.html' ?>
