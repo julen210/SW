74ab3caa665bab1a874b54535027ce1f5e2354d5
nachobf <nachobarriocanal@gmail.com>
2019-10-22 15:43:35 +0200

    showimage

--- js/ShowImageInForm.js
+++ js/ShowImageInForm.js
@@ -1,14 +1,5 @@
 var verImagen = function(event) {
-	/*if (input.files && input.files[0]) {
-         var reader = new FileReader();
-        reader.onload = function (e) {
-            $('#fotomostrar').remove();
-            $('#saltoFoto').remove();
-            $('<img id="fotomostrar" width = "100px" height = "100px"/><br id = "saltoFoto">').insertBefore($('#enviar'));
-            $('#fotomostrar').attr('src', e.target.result);           
-        };
-         reader.readAsDataURL(input.files[0]);
-    }*/
-	
-	$("#imagen").attr("src", URL.createObjectURL(event.target.files[0]));
+	$("<br><img id='imagen' name='imagen' width='100' src=''/>").insertAfter("#file");
+	var src = URL.createObjectURL(event.target.files[0]);
+	$("#imagen").attr("src", src);
 };
