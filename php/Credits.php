30ef0a6d585908d7d1b219a9b9abc568e636bb18
nachobf <nachobarriocanal@gmail.com>
2019-11-19 21:34:58 +0100

    Correcciones lab 6

--- php/Credits.php
+++ php/Credits.php
@@ -3,8 +3,13 @@
 <head>
   <?php include '../html/Head.html'?>
   <?php 
-	define("API_OWM","2c166a145ac561fb98d039878d560c06");	
+	define("API_OWM","2c166a145ac561fb98d039878d560c06");
 	$datoscliente = json_decode(file_get_contents('http://www.geoplugin.net/json.gp?ip='.$_SERVER['REMOTE_ADDR']),true);
+    $realIP = file_get_contents("http://ipecho.net/plain");
+	$datosservidor = json_decode(file_get_contents('http://ipinfo.io/'.$realIP.'/geo'),true);
+	$loc = explode(",", $datosservidor['loc']);
+	$latitudserver = $loc[0];
+	$longitudserver=$loc[1];
 ?>
 	<script src="../js/jquery-3.4.1.min.js"></script>
 	<script src="../js/ShowMap.js"></script>
@@ -29,12 +34,19 @@
 		<input class="spoilerbutton" type="button" value="Mostrar tu ubicación" onclick="this.value=this.value=='Mostrar tu ubicación'?'Ocultar tu ubicación':'Mostrar tu ubicación';">
 		<div class="spoiler"><div></div>
 		<?php
-			echo "<h2>Tu dirección IP se encuentra en:<br>";
+			echo "<h2>Tu dirección IP se encuentra ubicada en:<br>";
 			echo $datoscliente['geoplugin_city'].", ".$datoscliente['geoplugin_region'].", ".$datoscliente['geoplugin_countryName']."<br>";
 			echo "Latitud: ".$datoscliente['geoplugin_latitude']." <br> Longitud: ".$datoscliente['geoplugin_longitude']."</h2>";
 			echo "<br>";
 		?>
-		<div id="map" class="map"></div></div>
+		<div id="map" class="map"></div>
+	    <?php
+			echo "<h2>La dirección IP del servidor se encuentra ubicada en:<br>";
+			echo $datosservidor['city'].", ".$datosservidor['region'].", ".$datosservidor['country']."<br>";
+			echo "Latitud: ".$latitudserver." <br> Longitud: ".$longitudserver."</h2>";
+			echo "<br>";
+		?>
+		</div>
 		<br>
 		<input class="spoilerbutton" type="button" value="Mostrar previsión del tiempo en tu ubicación" onclick="this.value=this.value=='Mostrar previsión del tiempo en tu ubicación'?'Ocultar previsión del tiempo en tu ubicación':'Mostrar previsión del tiempo en tu ubicación';">
 		<div class="spoiler"><div>
@@ -43,7 +55,7 @@
 				if ( !is_numeric($temp) ) { return false; }
 				return round(($temp - 273.15));
 			}		
-			$datostiempo = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/forecast?daily&lat=".$datoscliente['geoplugin_latitude']."&lon=".$datoscliente['geoplugin_longitude']."&APPID=".API_OWM."&lang=es&cnt=40"),true);
+			$datostiempo = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/forecast?daily&lat=".$datoscliente['geoplugin_latitude']."&lon=".$datoscliente['geoplugin_longitude']."&APPID=".API_OWM."&lang=es&cnt=40"),true);
 			echo "<h2>Previsión para los próximos 5 días en ".$datoscliente['geoplugin_city'].", ".$datoscliente['geoplugin_region'].", ".$datoscliente['geoplugin_countryName']."</h2>";
 			echo "* Intervalos cada 3 horas.<br><br>";
 			echo"<div style='width:50%;height:500px;overflow-y:scroll;margin:auto;'>
@@ -62,7 +74,7 @@
 						<td>".$dia['dt_txt']."</td>
 						<td>".$temp_min."/".$temp_max."</td>
 						<td>".$dia['weather'][0]['description']."</td>
-						<td style='text-align:center;'> <img src='http://openweathermap.org/img/w/".$dia['weather'][0]['icon'].".png' height='50'/></td>
+						<td style='text-align:center;'> <img src='https://openweathermap.org/img/w/".$dia['weather'][0]['icon'].".png' height='50'/></td>
 					</tr>
 				";
 			}
